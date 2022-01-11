688e4f087217e33b45f0c1661b2168b88d601061
yasjpn <97491877+yasjpn@users.noreply.github.com>
2022-01-11 16:34:00 +0900

    state and navigate

--- src/Page1Default.jsx
+++ src/Page1Default.jsx
@@ -1,12 +1,22 @@
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 
 export const Page1Default = () => {
+  const arr = [...Array(100).keys()];
+
+  const navigate = useNavigate();
+
+  const onClickDetailA = () => navigate("/page1/detaila", { state: arr });
+
   return (
     <>
       <h2>Page1(Default)</h2>
-      <Link to="/page1/detaila">DetailA</Link>
+      <Link to="/page1/detaila" state={{ state: arr }}>
+        DetailA
+      </Link>
       <br />
       <Link to="/page1/detailb">DetailB</Link>
+      <br />
+      <button onClick={onClickDetailA}>DetailA</button>
     </>
   );
 };
--- src/Page1DetailA.jsx
+++ src/Page1DetailA.jsx
@@ -1,4 +1,9 @@
+import { useLocation } from "react-router-dom";
+
 export const Page1DetailA = () => {
+  const { state } = useLocation();
+  console.log(state);
+
   return (
     <div>
       <h3>Page1 detail A</h3>
