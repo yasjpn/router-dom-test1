30817cc84afc6b892371098ab4763239f7329094
yasjpn <97491877+yasjpn@users.noreply.github.com>
2022-01-11 16:43:36 +0900

    go back

--- src/Page1DetailA.jsx
+++ src/Page1DetailA.jsx
@@ -1,12 +1,15 @@
-import { useLocation } from "react-router-dom";
+import { useLocation, useNavigate } from "react-router-dom";
 
 export const Page1DetailA = () => {
   const { state } = useLocation();
   console.log(state);
 
+  const navigate = useNavigate();
+
   return (
     <div>
       <h3>Page1 detail A</h3>
+      <button onClick={() => navigate(-1)}>Go Back</button>
     </div>
   );
 };
