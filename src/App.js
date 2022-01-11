e27277cb5551ee3b63f64cdbf9a41ae111bb93df
yasjpn <97491877+yasjpn@users.noreply.github.com>
2022-01-11 15:17:09 +0900

    router

--- src/App.js
+++ src/App.js
@@ -1,11 +1,6 @@
-import { BrowserRouter, Link, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Link } from "react-router-dom";
 
-import { Home } from "./Home";
-import { Page1 } from "./Page1";
-import { Page1Default } from "./Page1Default";
-import { Page1DetailA } from "./Page1DetailA";
-import { Page1DetailB } from "./Page1DetailB";
-import { Page2 } from "./Page2";
+import { Router } from "./router/Router";
 import "./styles.css";
 
 export default function App() {
@@ -18,15 +13,7 @@ export default function App() {
         <br />
         <Link to="/page2">Page2</Link>
       </div>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="page1" element={<Page1 />}>
-          <Route index element={<Page1Default />} />
-          <Route path="detaila" element={<Page1DetailA />} />
-          <Route path="detailb" element={<Page1DetailB />} />
-        </Route>
-        <Route path="page2" element={<Page2 />} />
-      </Routes>
+      <Router />
     </BrowserRouter>
   );
 }
---
+++ src/router/Router.jsx
@@ -0,0 +1,22 @@
+import { Routes, Route } from "react-router-dom";
+
+import { Home } from "../Home";
+import { Page1 } from "../Page1";
+import { Page1Default } from "../Page1Default";
+import { Page1DetailA } from "../Page1DetailA";
+import { Page1DetailB } from "../Page1DetailB";
+import { Page2 } from "../Page2";
+
+export const Router = () => {
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="page1" element={<Page1 />}>
+        <Route index element={<Page1Default />} />
+        <Route path="detaila" element={<Page1DetailA />} />
+        <Route path="detailb" element={<Page1DetailB />} />
+      </Route>
+      <Route path="page2" element={<Page2 />} />
+    </Routes>
+  );
+};
